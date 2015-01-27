28ea9a5565221fd724e781897d0a7fb1aa1b9522
Von Random <von@vdrandom.org>
2015-01-27 10:06:40 +0300

    got rid of xkb fix in rc.lua, moved it to autostart, fixed history for zsh and added autostart

--- rc.lua
+++ rc.lua
@@ -662,8 +662,6 @@ client.connect_signal('unfocus', function(c) c.border_color = beautiful.border_n
 -- {{{ Autostart
 -- don't forget you sync this file
 -- this shit runs every time you restart your wm, dumbass.
----- apply the workaround to make mouse work outside en-us keyboard layout
-awful.util.spawn_with_shell('xkbcomp $DISPLAY - | egrep -v "group . = AltGr;" | xkbcomp - $DISPLAY')
 ---- set keyboard layouts
 awful.util.spawn_with_shell('setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,compose:menu,grp_led:scroll')
 ---- populate xrdb with .Xresources config
