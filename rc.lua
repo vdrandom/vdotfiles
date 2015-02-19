d3d907d05786c428d210882bf48c2d9c004b57e4
Von Random <von@mechanus.net>
2015-02-19 12:17:04 +0300

    gimme back my compose menu key

--- rc.lua
+++ rc.lua
@@ -663,7 +663,7 @@ client.connect_signal('unfocus', function(c) c.border_color = beautiful.border_n
 -- don't forget you sync this file
 -- this shit runs every time you restart your wm, dumbass.
 ---- set keyboard layouts
-awful.util.spawn_with_shell('setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,grp_led:scroll')
+awful.util.spawn_with_shell('setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,grp_led:scroll,compose:menu')
 ---- populate xrdb with .Xresources config
 awful.util.spawn_with_shell('xrdb /home/von/.Xresources')
 ---- execute all the other shit, installation specific
