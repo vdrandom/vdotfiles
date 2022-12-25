aeb129a62101ad08a1ea5b34339e582ac90a96f0
Von Random <von@vdrandom.org>
2022-12-25 03:36:59 +0200

    vim and nvim config refactor / update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,36 +1,40 @@
-require('paq') {
-    'savq/paq-nvim',
+local ensure_packer = function()
+    local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+        vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
 
-    'mhinz/vim-signify',
-    'tpope/vim-commentary',
-    'tpope/vim-fugitive',
-    'tpope/vim-rsi',
-    'tpope/vim-vinegar',
-    'hoob3rt/lualine.nvim',
-    'w0rp/ale',
+local packer_bootstrap = ensure_packer()
 
-    'lifepillar/vim-gruvbox8',
-    'lifepillar/vim-solarized8',
-    'NLKNguyen/papercolor-theme'
-}
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'lifepillar/vim-gruvbox8'
 
-require('lualine').setup{
-    options = {
-        icons_enabled        = false,
-        section_separators   = { left = '', right = ''},
-        component_separators = { left = '', right = ''}
+    use 'mhinz/vim-signify'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rsi'
+    use 'tpope/vim-vinegar'
+    use 'hoob3rt/lualine.nvim'
+    use {
+        'w0rp/ale',
+        ft = { 'sh', 'zsh', 'lua', 'python' },
+        cmd = 'ALEEnable'
     }
-}
+end)
 
-vim.g.vimwiki_list = {{path='$HOME/vimwiki/', syntax='markdown', ext='.md'}}
-vim.g.PaperColor_Theme_Options = {
-    theme = {
-        ["default.dark"]  = { allow_bold = 1, allow_italic = 1 },
-        ["default.light"] = { allow_bold = 1, allow_italic = 1 }
-    }
-}
+if packer_bootstrap then
+    require('packer').sync()
+    return
+end
 
+require('lualine').setup { options = { icons_enabled = false } }
 vim.o.bg = 'dark'
 vim.o.termguicolors = true
+vim.g.gruvbox_transp_bg          = 1
+vim.g.gruvbox_plugin_hi_groups   = 1
 vim.g.gruvbox_filetype_hi_groups = 1
 vim.cmd('colorscheme gruvbox8')
--- vimrc
+++ vimrc
@@ -1,3 +1,4 @@
+vim9script
 set nobackup nomodeline backspace=indent,eol,start foldmethod=marker cursorline
 set list listchars=tab:-->,nbsp:x,trail:*
 set hlsearch incsearch ignorecase smartcase
@@ -6,32 +7,31 @@ set tabstop=3 softtabstop=4 shiftwidth=4 smarttab expandtab autoindent
 set wildmenu showcmd ruler laststatus=2 mouse= t_ut= guicursor= t_Co=256
 set belloff=all colorcolumn=80 formatoptions+=j relativenumber breakindent
 set keymap=russian-jcukenwintype iminsert=0 imsearch=0
-let &statusline="[%F] %R%H%W%M %=[%{&fenc}/%{&ff}] %y [%4l/%L:%3v]"
-let g:sh_indent_case_labels = 1
+&statusline = "[%F] %R%H%W%M %=[%{&fenc}/%{&ff}] %y [%4l/%L:%3v]"
+g:sh_indent_case_labels = 1
 
-" mappings
+# mappings
 map <Space> <NOP>
-let mapleader="\<Space>"
-noremap <F1> <Esc>
+var mapleader = '\<Space>'
+noremap  <F1> <Esc>
+noremap  <Leader>/ :noh<CR>
+noremap  <Leader>y "+y
+noremap  <Leader>d "+d
+noremap  <Leader>p "+p
+noremap  <Leader>P "+P
+noremap  q <NOP>
 noremap! <F1> <Esc>
+noremap! <C-@> <C-^>
+noremap! <C-Space> <C-^>
 nnoremap <Leader>l :setlocal list!<CR>
 nnoremap <Leader>c :setlocal cursorline!<CR>
 nnoremap <Leader>w :setlocal wrap!<CR>
-noremap <Leader>/ :noh<CR>
-noremap <Leader>y "+y
-noremap <Leader>d "+d
-noremap <Leader>p "+p
-noremap <Leader>P "+P
-noremap q <NOP>
-noremap! <C-@> <C-^>
-noremap! <C-Space> <C-^>
 
-" plugins
-let plugrc = expand('$HOME/.vimplugrc')
-let plugdir = expand('$HOME/.vim/plugged')
-let plug = plugdir . '/vim-plug/plug.vim'
-if filereadable(plugrc) && filereadable(plug) && v:version >= 703
-    execute 'source' fnameescape(plugrc)
+g:plugrc  = expand('$HOME/.vimplugrc')
+g:plugdir = expand('$HOME/.vim/plugged')
+g:plug    = g:plugdir .. '/vim-plug/plug.vim'
+if filereadable(g:plugrc) && filereadable(g:plug) && v:version >= 703
+    execute 'source' fnameescape(g:plugrc)
 endif
 
 syntax on
