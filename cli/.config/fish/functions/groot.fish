9339c7a5349d2d6a07d6fd9236a7ac1cedb12968
Von Random <von@vdrandom.org>
2023-03-17 19:00:07 +0200

    fish: fix groot function to fail quietly

--- cli/.config/fish/functions/groot.fish
+++ cli/.config/fish/functions/groot.fish
@@ -1,4 +1,6 @@
 function groot
+    set -l is_git_worktree (command git rev-parse --is-inside-work-tree)
+    string match -qe 'true' "$is_git_worktree" || return
     set -l root (command git rev-parse --show-toplevel)
     test $status -eq 0 && cd $root
 end
