38a8bca89cb7a03467b5fac6a22d7993b893d5a1
Von Random <von@vdrandom.org>
2023-06-19 14:42:15 +0300

    fish: make grc optional

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -6,15 +6,17 @@ if status is-interactive
         end
     end
 
-    set -l grc_cmds \
-        iptables ipneighbor ipaddr iproute ip nmap netstat \
-        traceroute tcpdump ss ping \
-        dockerversion dockersearch dockerpull dockerps dockernetwork \
-        docker-machinels dockerinfo dockerimages \
-        lspci lsof lsmod lsblk lsattr getfacl id whois vmstat ulimit \
-        systemctl sysctl stat pv ps ping last gcc free findmnt fdisk env du \
-        dig diff df blkid
-    for cmd in $grc_cmds
-        command -q $cmd && alias $cmd="command grc -es --colour=auto $cmd"
+    if command -q grc
+        set -l grc_cmds \
+            iptables ipneighbor ipaddr iproute ip nmap netstat \
+            traceroute tcpdump ss ping \
+            dockerversion dockersearch dockerpull dockerps dockernetwork \
+            docker-machinels dockerinfo dockerimages \
+            lspci lsof lsmod lsblk lsattr getfacl id whois vmstat ulimit \
+            systemctl sysctl stat pv ps ping last gcc free findmnt fdisk env du \
+            dig diff df blkid
+        for cmd in $grc_cmds
+            command -q $cmd && alias $cmd="command grc -es --colour=auto $cmd"
+        end
     end
 end
