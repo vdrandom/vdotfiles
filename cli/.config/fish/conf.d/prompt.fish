db644c5a15458936aa73c9e74e29437755e0615d
Von Random <von@mechanus.net>
2023-06-20 01:39:32 +0300

    fish: compatibility prompt

--- cli/.config/fish/conf.d/prompt.fish
+++ cli/.config/fish/conf.d/prompt.fish
@@ -1,6 +1,12 @@
-set prompt_sep \ue0b0
-set prompt_bang \n\ (set_color brred)\u266a\ 
-set git_sign \ue0a0
+if ! string match -eq linux $TERM
+    set prompt_sep \ue0b0
+    set prompt_bang \n\ (set_color brred)\u266a\ 
+    set git_sign \ue0a0
+else
+    set prompt_bang \n(set_color brred) \#\ 
+    set git_sign g
+end
+
 set color_fg brwhite
 set color_git_branch 3c3c3c
 set color_git yellow blue red purple
