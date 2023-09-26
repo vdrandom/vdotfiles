bfded8cd9f46be557d17980eae1b722b2a57b7a7
Von Random <von@mechanus.net>
2023-09-26 09:31:22 +0300

    nvim: refactor bootstrap

--- cli/.config/nvim/lua/plugins.lua
+++ cli/.config/nvim/lua/plugins.lua
@@ -1,12 +1,8 @@
 -- [[ plugins bootstrap ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", lazypath
-    }
-end
+local lazyurl = "https://github.com/folke/lazy.nvim.git"
+local lazycmd = { "git", "clone", "--filter=blob:none", "--branch=stable", lazyurl, lazypath }
+if not vim.loop.fs_stat(lazypath) then vim.fn.system(lazycmd) end
 vim.opt.rtp:prepend(lazypath)
 
 --[[ plugins list ]]
