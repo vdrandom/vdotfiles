5b69463310ca9b4c52ed2b2c59eecf9fca58d179
Von Random <von@mechanus.net>
2026-03-16 13:08:54 +0200

    nvim: neovide font size on macos

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -26,5 +26,5 @@ vim.o.iminsert = 0
 vim.o.imsearch = 0
 
 local fsize = '11'
-if vim.loop.os_uname().sysname == 'Darwin' then fsize = '14' end
+if vim.loop.os_uname().sysname == 'Darwin' then fsize = '15' end
 vim.o.guifont = 'Maple Mono NL NF:h' .. fsize
