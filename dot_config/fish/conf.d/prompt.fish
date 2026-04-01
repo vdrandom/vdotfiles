dc69b1825bcbd8756928a5e1a85c9e17128eaf13
Von Random <von@mechanus.net>
2026-04-01 23:53:31 +0300

    fish: async git status, reorganize functions

--- dot_config/fish/conf.d/prompt.fish
+++ dot_config/fish/conf.d/prompt.fish
@@ -1,10 +1,61 @@
-set bang_symbol \u276f
-set kube_symbol \u2388
-set kube_config "$HOME/.kube/config"
-set git_symbol  \ue0a0
-set color_git_branch brblack
+set prompt_bang_symbol \u276f
+set prompt_kube_symbol \u2388
+set prompt_kube_config "$HOME/.kube/config"
+set prompt_color_reset "$(set_color normal)"
 
-# git symbols and colors
-# [1] unstaged, [2] staged, [3] untracked, [4] conflicts
-set git_status \~ + ! \*
-set color_git  yellow blue red purple
+function fish_prompt
+    set -g prompt_string
+    set -g prev_color
+    fish_prompt.add \[ brblack
+    fish_prompt.user
+    fish_prompt.pwd
+    fish_prompt.kube
+    fish_prompt.git
+    fish_prompt.add \] brblack
+    fish_prompt.bang
+
+    echo "$prompt_string"
+    set -e prompt_string
+end
+
+function fish_prompt.add
+    set -l text $argv[1]
+    if test -n $argv[2]
+        set -l color "$(set_color $argv[2])"
+        set value "$color$text$prompt_color_reset"
+    else
+        set value "$text"
+    end
+    if test -z "$prompt_string"
+        set prompt_string "$value"
+    else
+        set -a prompt_string "$value"
+    end
+end
+
+function fish_prompt.bang
+    if ! string match -eq linux "$TERM"
+        fish_prompt.add \n$prompt_bang_symbol\  brblack
+    else
+        fish_prompt.add \n\#\  brred
+    end
+end
+
+function fish_prompt.kube
+    if ! test -r "$prompt_kube_config"
+        return
+    end
+    fish_prompt.add "$prompt_kube_symbol"
+    set -l kube_context "$(awk '($1 == "current-context:") {print $2}' "$prompt_kube_config")"
+    fish_prompt.add "$kube_context" green
+end
+
+function fish_prompt.pwd
+    fish_prompt.add (prompt_pwd) blue
+end
+
+function fish_prompt.user
+    if test -n "$SSH_CONNECTION" || string match -qe root "$USER"
+        fish_prompt.add "$USER@$hostname" brblack
+    end
+end
--- dot_config/fish/functions/fish_prompt.kube.fish
+++
@@ -1,8 +0,0 @@
-function fish_prompt.kube
-    if ! test -r "$kube_config"
-        return
-    end
-    fish_prompt.add "$kube_symbol"
-    set -l kube_context (awk '($1 == "current-context:") {print $2}' "$kube_config")
-    fish_prompt.add "$kube_context" green
-end
